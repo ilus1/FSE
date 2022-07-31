f5c0b77baf1121bd650f4e4a353a1355ccb06f63
ilus1 <ilusdasilva@gmail.com>
2022-07-30 20:50:41 -0400

    Adicionado mapeamendo dos 2 cruzamentos

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,11 @@
 
-    short unsigned int semaforoAux[3] = {31, 25, 29};
-    short unsigned int semaforoMain[3] = {28, 27, 26};
+    short unsigned int semaforoAux[2][3] = {{8, 9, 14}, {31, 25, 29}};
+    short unsigned int semaforoMain[2][3] = {{30, 21, 22}, {28, 27, 26}};
     short unsigned int auxMinGreenTime = 5000;
     short unsigned int mainMinGreenTime = 10000;
-    short unsigned int pedestreAux = 10;
-    short unsigned int pedestreMain = 11;
-    short unsigned int sensorCarroNorte = 15;
-    short unsigned int sensorCarroSul = 16;
-    short unsigned int velocidadeLeste[2] = {1, 4};
-    short unsigned int velocidadeOeste[2] = {5, 6};
+    short unsigned int pedestreAux[2] = {12, 10};
+    short unsigned int pedestreMain[2] = {13, 11};
+    short unsigned int sensorCarroNorte[2] = {7, 15};
+    short unsigned int sensorCarroSul[2] = {0, 16};
+    short unsigned int velocidadeLeste[2][2] = {{2, 3}, {1, 4}};
+    short unsigned int velocidadeOeste[2][2] = {{23, 24}, {5, 6}};
